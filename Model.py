afd9d086694be6d26cb740b311a2c8adcbd026bc
sornsook <46738736+sornsook@users.noreply.github.com>
2019-05-06 15:31:27 +0900

    Update Model.py

--- Model.py
+++ Model.py
@@ -182,7 +182,7 @@ def VGG16(pretrained_weights = None,input_size = (480,640,3)):
     return model,model_name
 
 def unet(pretrained_weights = None,input_size = (480,640,3)):
-    
+    '''
     inputs = Input(input_size)
     conv1_1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
     conv1_2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv1_1)
@@ -271,7 +271,7 @@ def unet(pretrained_weights = None,input_size = (480,640,3)):
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
-    '''
+    
     model = Model(input = inputs, output = conv10)
 
 #    model.compile(optimizer = optimizers.Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
